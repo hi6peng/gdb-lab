1ac71ca68e107b52ec74bf12a9fceb29466e80c5
6peng <hi6peng@qq.com>
2024-12-07 21:26:08 +0800

    add String Class Debugging in GDB
    http://www.yolinux.com/TUTORIALS/LinuxTutorialC++StringClass.html

---
+++ string-class/testprog.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+// Helper routine ps to print a string class variable.
+
+void ps(string& s){ cout << s << endl; }
+
+int main()
+{
+   string a("String A");
+   string b;
+
+   b = "String B";
+
+   cout << "Hello!" << endl;
+}
